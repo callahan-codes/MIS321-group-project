6bd7ad3a2df61e35a4052a9cd71942e0690d1310
callahan-codes <bryceccode@gmail.com>
2024-11-11 09:24:09 -0600

    assembly info push

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7479fca1e0487a357bc9f0323f4e0551534574")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b638b2b4a17d77fcd694a55a2d20e29d9f7dc2b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
