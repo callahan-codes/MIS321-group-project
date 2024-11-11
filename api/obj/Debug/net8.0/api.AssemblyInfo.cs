7d7479fca1e0487a357bc9f0323f4e0551534574
callahan-codes <bryceccode@gmail.com>
2024-11-10 22:38:20 -0600

    branch update

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4a0bff6c1d7d9bb5027d0fd985a7cf87be157c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0d6c20572217e029a52eed7df49944cc999a5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
