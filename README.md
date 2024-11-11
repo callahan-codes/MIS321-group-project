b638b2b4a17d77fcd694a55a2d20e29d9f7dc2b7
callahan-codes <bryceccode@gmail.com>
2024-11-11 08:39:24 -0600

    readme update

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # MIS321-group-project
-<h1>Bryce's Branch</h1>
-<p>testing to make sure this applies to my branch only.</p>
-
 <hr/>
 <h1>Requirements</h1>
 <p><strong>Make sure yall save the <u>pulled repository</u> to your local machine <u>before making any changes and pushing them</u>. We all need to be responsible for having an extra non-edited project folder everytime we pull from the repo!!! This is so if anyone pushes a project with errors, we have the backup.</strong></p>
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0d6c20572217e029a52eed7df49944cc999a5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7479fca1e0487a357bc9f0323f4e0551534574")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
