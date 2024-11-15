4c474c4ffd22c1fb7889e5e6657574f894e24a27
callahan-codes <bryceccode@gmail.com>
2024-11-15 16:02:01 -0600

    push before merged to merged-testing

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356dff9a0ad7d14c41719885dd67bdd9f41f2773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7271f20da612548ae10ecd982697199908f57952")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
