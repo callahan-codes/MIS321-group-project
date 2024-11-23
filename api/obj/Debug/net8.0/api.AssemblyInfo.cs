33000bf620398ca11553ed803b16106e305f109c
callahan-codes <bryceccode@gmail.com>
2024-11-23 11:26:48 -0600

    updated code

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c25237bc71a9811ab71a720f7be4a5067da6496")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a939bcbd59d4786dec448ef792969090062a242")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
